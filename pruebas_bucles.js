f886141944addcd40838ff32d6b7bcd4a530c206
Japon341 <146208083+Japon341@users.noreply.github.com>
2024-05-30 01:12:53 +0200

    Add files via upload

---
+++ pruebas_bucles.js
@@ -0,0 +1,63 @@
+/*bucle con WHILE;
+ Creame un bucle en el que cuando la variable i  sea -=5 se le vaya sumando 1.
+
+*/
+let i = 0; 
+while ( i <= 5 ){
+    console.log("Valor de i", i);
+
+    i = i + 1 ;
+}
+
+
+//BUCLE CON DO WHILE;
+let DW = 5;
+
+do {
+  console.log("Hola a todos");
+  DW = DW + 1;
+} while (DW < 5);
+
+console.log("Bucle finalizado");
+
+
+
+
+//BUCLE CON FOR 
+
+/*let myMap = new Map([[ "Jaime", 20], ["Manuel", 21], ["Zaragoza", 2 ] ])
+let productsByName = new Map( [["Madrid Pierde"], ["Jesús Maria"], ["Palomo"] ]);
+
+
+for (const value of myMap) {
+    let a = productsByName.get(value)
+    console.log(a)
+}*/
+
+
+
+
+
+
+
+
+//Haz una lista de números del 1 al 10 mezclados y haz una función que te lo ordene de mayor a menor, y en una variable auxiliar guarda el máximo valor de esa lista, todo en un bucle.
+
+
+
+
+
+
+//
+
+//Ordenación burbuja.
+
+
+
+
+
+//
+
+
+
+
